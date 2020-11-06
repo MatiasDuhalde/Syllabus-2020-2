ee2a8468619033b326bb1ad125eac227625aea13
Matias Duhalde <matias.duhalde@uc.cl>
2020-11-06 18:40:24 +0000

    Add C2 IIC2413 files

--- Controles/Control 2/database.py
+++ Controles/Control 2/database.py
@@ -15,6 +15,7 @@ Buffer = Buffer()
 # The relation dictionary indexed by relation name
 Relations = {}
 
+
 # This is what 'CREATE TABLE R(a int, b int, c text)' does here attributes = ['a','b','c'], types = ['int','int','str']
 # buff is the buffer that the table has access to; this is set to be the one used by the database
 def create_table(rName, attributes, types, buff = Buffer):
@@ -31,6 +32,7 @@ def create_table(rName, attributes, types, buff = Buffer):
     # If all went well e return True
     return True
 
+
 # This is what 'SELECT * FROM R' does
 def all_tuples(R):
     R = Relations[R]
@@ -42,20 +44,49 @@ def all_tuples(R):
 
     R.close()
 
+
 # This is what 'SELECT * FROM R WHERE a = x' does
-def filtered_tuples(R,cond):
+def filtered_tuples(R, cond):
     # Here cond is just a single attribute equality
     # cond is if the form 'attr = value'
 
-    pass
+    R = Relations[R]
+
+    attr, value = cond.split('=')
+    # remove trailing and leading spaces in case cond looks like 'attr = value'
+    attr = attr.strip()
+    value = value.strip()
+
+    # Check if attr is in relation
+    R_attributes = R.get_attribute_types()
+    if not attr in R_attributes:
+        print(f'{attr} is not a valid attribute for {R.rName}')
+        return
+
+    # If type is integer, convert (else is string)
+    if R_attributes[attr] == 'int':
+        if value.isdecimal():
+            value = int(value)
+        else:
+            print(f'{value} is not an int')
+            return
+
+    # Check every tuple
+    R.open()
+    while (R.has_next()):
+        tup = R.next()
+        if R.get_individual_values(tup)[attr] == value:
+            print(tup)
+    R.close()
+
 
 def cross(R, S):
     # Performs the cross product of the two relations
     # Assumes that R is not equal to S, otherwise it will not work properly -- for bonus, implement iterators properly
 
 
-    R = Relations[R]
-    S = Relations[S]
+    R = Relations[R].get_iterator()
+    S = Relations[S].get_iterator()
 
     R.open()
 
@@ -71,32 +102,103 @@ def cross(R, S):
     # End of the cross product
     return True
 
-# SELECT * FROM R,S WHERE R.attrib = S.attrib
-def nested_loop_join(R,S,attrib):
-    # Performs a join on the attribute attrib
-    pass
 
+# SELECT * FROM R,S WHERE R.attrib = S.attrib
+def nested_loop_join(R, S, attrib):
+    # Performs a join on the attribute attrib
+    R = Relations[R]
+    S = Relations[S]
+
+    # Check if attr is in relation
+    R_attributes = R.get_attribute_types()
+    S_attributes = S.get_attribute_types()
+    if not attrib in R_attributes:
+        print(f'{attrib} is not a valid attribute for {R.rName}')
+        return
+    if not attrib in S_attributes:
+        print(f'{attrib} is not a valid attribute for {S.rName}')
+        return
+
+    # Check every tuple combination
+    R.open()
+    while (R.has_next()):
+        tupR = R.next()
+        S.open()
+        while (S.has_next()):
+            tupS = S.next()
+            if R.get_individual_values(tupR)[attrib] == S.get_individual_values(tupS)[attrib]:
+                print(tupR,tupS)
+        S.close()
+    R.close()
 
 
 # SELECT * FROM R,S WHERE R.attrib = S.attrib
 def block_nested_loop_join(R,S,attrib):
     # For bonus b)
-    pass
+    R = Relations[R]
+    S = Relations[S]
+
+    # Check if attr is in relation
+    R_attributes = R.get_attribute_types()
+    S_attributes = S.get_attribute_types()
+    if not attrib in R_attributes:
+        print(f'{attrib} is not a valid attribute for {R.rName}')
+        return
+    if not attrib in S_attributes:
+        print(f'{attrib} is not a valid attribute for {S.rName}')
+        return
+
+    # Check every tuple combination
+    # NOTE: All relations share same buffer
+    # Fill every frame of the buffer but one with R pages
+    R.buffer.flush_and_fill(R.root_page, R.buffer.buffSize - 1)
+    next_R_page = R.buffer.frames[R.buffer.buffSize - 2].next
+    # Fill last frame with S page
+    R.buffer.fetch_into_frame(S.root_page, R.buffer.buffSize - 1)
+    next_S_page = R.buffer.frames[R.buffer.buffSize - 1].next
+    for i in R.buffer.frames:
+        print(i.pname)
+
+    print(next_R_page, next_S_page)
+
+    S.open()
+    while True:
+        tupS = S.next()
+        R.open()
+        while (True):
+            tupR = R.next()
+            if S.get_individual_values(tupS)[attrib] == R.get_individual_values(tupR)[attrib]:
+                print(tupS,tupR)
+            if R.current_page == next_R_page:
+                break
+        if S.current_page == next_S_page:
+            S_next_page = S.current_page.get_next_page()
+            R_next_page = R.current_page.get_next_page()
+            if S_next_page:
+                R.fetch_into_frame(S_next_page, R.buffer.buffSize - 1)
+            elif R_next_page:
+                R.buffer.flush_and_fill(R.current_page, R.buffer.buffSize - 1)
+            else:
+                break
+
+    S.close()
+    R.close()
 
 
 # Test cases:
-'''
-create_table('R',['a','b'],['int','int'])
+if __name__ == "__main__":
+    create_table('R',['a','b'],['int','int'])
 
-create_table('S',['b','c'],['int','int'])
+    create_table('S',['b','c'],['int','int'])
 
-cross('R','S')
+    # cross('R', 'R')
 
-all_tuples('R')
+    # all_tuples('R')
 
-all_tuples('S')
+    # all_tuples('S')
 
-filtered_tuples('R','a = 1')
+    # filtered_tuples('R', 'a = 1')
 
-nested_loop_join('R','S','b')
-'''
+    # nested_loop_join('R', 'S', 'b')
+
+    block_nested_loop_join('R', 'S', 'b')
--- Controles/Control 2/relation.py
+++ Controles/Control 2/relation.py
@@ -1,179 +1,259 @@
 from pageNew import *
 from buffer import *
 class Relation:
-	# The constructor initializes the buffer
-	def __init__(self, rName, aNames, aTypes, buff):
-		if not(len(aNames) == len(aTypes)):
-			print('Relation definition error')
-			return
+    # The constructor initializes the buffer
+    def __init__(self, rName, aNames, aTypes, buff):
+        if not(len(aNames) == len(aTypes)):
+            print('Relation definition error')
+            return
 
-		self.buffer = buff # The buffer that the relation uses; this is passed to the relation by the database
-		self.rName = rName # Relation name
-		self.attributes = aNames # Names of our attributes; this should be a list
-		self.types = aTypes # Type of each attribute; for now we support only int and text, where text can not have spaces
-		self.numAttrib = len(self.attributes) # number of attributes
-		self.root_page = self.rName + '-1' # Name of the Root page = relationName-1 (e.g. for R R-1)
-		# We make sure that the root page exists		
-		self.create_root()
-		self.current_page = None # The page we are currently on; before operning we are not doing anything
-		self.current_pos = None # Current position on the current_page; before opening None
+        self.buffer = buff # The buffer that the relation uses; this is passed to the relation by the database
+        self.rName = rName # Relation name
+        self.attributes = aNames # Names of our attributes; this should be a list
+        self.types = aTypes # Type of each attribute; for now we support only int and text, where text can not have spaces
+        self.numAttrib = len(self.attributes) # number of attributes
+        self.root_page = self.rName + '-1' # Name of the Root page = relationName-1 (e.g. for R R-1)
+        # We make sure that the root page exists        
+        self.create_root()
+        self.current_page = None # The page we are currently on; before operning we are not doing anything
+        self.current_pos = None # Current position on the current_page; before opening None
 
-	def create_root(self):
-		# Next, we create the root page if it does not exist already (not to have inconsistencies later)
-		# Note that the only communication with the disk is through the buffer
-		self.buffer.create_page(self.root_page)
-
-
-	def open(self):
-		# Resets the cursor to the beginning
-		self.current_page = self.root_page
-		self.current_pos = -1
-
-	def has_next(self):
-		# Returns True/False depending on whether there is a next tuple
-		# It does not modify self.current_page, nor self.current_pos
-		# The next function does this looping recursively
-		return self.find_next_pos(self.current_page,self.current_pos)
-
-	def find_next_pos(self, currPage, currPos):
-		# Uses the same logic as get_next_pos(), but without changing self.current_page, nor self.current_pos
-
-		if not(currPage):
-			return False
-
-		# We need to advance by one first and do all the checks (we consumed the last tuple in next())
-		currPos += 1
-
-		# If we came to the end, we need to move onto the next page; 4 should be changed by PAGE_SIZE constant
-		if currPos == 5:
-			# Get the page we are working with
-			page = self.buffer.get_page(currPage)
-			next_page = page.next
-			if next_page == '<NULL>':
-				# We came to an end
-				return False
-			else:
-				# If not, we obtain the next page name
-				currPage = next_page
-				# Set the position to zero
-				currPos = 0
-
-		# Now we roll
-
-		page = self.buffer.get_page(currPage)
-
-		# If the page position is empty, we call has_next again recursively:
-		if page.get_element_from_page(currPos) == '<EMPTY>':
-			return self.find_next_pos(currPage,currPos)
-		
-		return True
+    def create_root(self):
+        # Next, we create the root page if it does not exist already (not to have inconsistencies later)
+        # Note that the only communication with the disk is through the buffer
+        self.buffer.create_page(self.root_page)
 
 
-	def get_next_pos(self):
-		# returns true if there is a next tuple
-		# It also positions the "pointers" self.current_page and self.current_tuple over the next tuple
-		# That is, it will advance the self.current_page and self.current_pos to the one with the next tuple
+    def open(self):
+        # Resets the cursor to the beginning
+        self.current_page = self.root_page
+        self.current_pos = -1
 
-		if not(self.current_page):
-			print('Iterator is closed')
-			return False
+    def has_next(self):
+        # Returns True/False depending on whether there is a next tuple
+        # It does not modify self.current_page, nor self.current_pos
+        # The next function does this looping recursively
+        return self.find_next_pos(self.current_page,self.current_pos)
 
-		# We need to advance by one first and do all the checks (we consumed the last tuple in next())
-		self.current_pos += 1
+    def find_next_pos(self, currPage, currPos):
+        # Uses the same logic as get_next_pos(), but without changing self.current_page, nor self.current_pos
 
-		# If we came to the end, we need to move onto the next page; 4 should be changed by PAGE_SIZE constant
-		if self.current_pos == 5:
-			# Get the page we are working with
-			page = self.buffer.get_page(self.current_page)
-			next_page = page.next
-			if next_page == '<NULL>':
-				# We came to an end
-				return False
-			else:
-				# If not, we obtain the next page name
-				self.current_page = next_page
-				# Set the position to zero
-				self.current_pos = 0
+        if not(currPage):
+            return False
 
-		# Now we roll
-		pname = self.current_page
-		pos = self.current_pos
+        # We need to advance by one first and do all the checks (we consumed the last tuple in next())
+        currPos += 1
 
-		page = self.buffer.get_page(pname)
+        # If we came to the end, we need to move onto the next page; 4 should be changed by PAGE_SIZE constant
+        if currPos == 5:
+            # Get the page we are working with
+            page = self.buffer.get_page(currPage)
+            next_page = page.next
+            if next_page == '<NULL>':
+                # We came to an end
+                return False
+            else:
+                # If not, we obtain the next page name
+                currPage = next_page
+                # Set the position to zero
+                currPos = 0
 
-		# If the page position is empty, we call has_next again recursively:
-		if page.get_element_from_page(pos) == '<EMPTY>':
-			return self.get_next_pos()
-		
-		return True
+        # Now we roll
 
+        page = self.buffer.get_page(currPage)
 
-		
-	def next(self):
-		# returns the next tuple; if it does not exist returns none
-		# When called, will advance the self.current_page and self.current_pos pointers
-		if (self.get_next_pos()):
-			pname = self.current_page
-			page = self.buffer.get_page(pname)
-			return page.get_element_from_page(self.current_pos)
-		return None
+        # If the page position is empty, we call has_next again recursively:
+        if page.get_element_from_page(currPos) == '<EMPTY>':
+            return self.find_next_pos(currPage,currPos)
+        
+        return True
 
 
-	def close(self):
-		# Closes the iterator
-		self.current_page = None
-		self.current_pos = None
+    def get_next_pos(self):
+        # returns true if there is a next tuple
+        # It also positions the "pointers" self.current_page and self.current_tuple over the next tuple
+        # That is, it will advance the self.current_page and self.current_pos to the one with the next tuple
 
-	def get_attribute_types(self):
-		# Returns a dictionary of attribute name: attribute type
-		# E.g. if we have R(a int, b str), it will return {'a':'int','b':'str'}
+        if not(self.current_page):
+            print('Iterator is closed')
+            return False
 
-		res = {}
+        # We need to advance by one first and do all the checks (we consumed the last tuple in next())
+        self.current_pos += 1
 
-		for i in range(self.numAttrib):
-			res[self.attributes[i]] = self.types[i]
+        # If we came to the end, we need to move onto the next page; 4 should be changed by PAGE_SIZE constant
+        if self.current_pos == 5:
+            # Get the page we are working with
+            page = self.buffer.get_page(self.current_page)
+            next_page = page.next
+            if next_page == '<NULL>':
+                # We came to an end
+                return False
+            else:
+                # If not, we obtain the next page name
+                self.current_page = next_page
+                # Set the position to zero
+                self.current_pos = 0
 
-		return res
+        # Now we roll
+        pname = self.current_page
+        pos = self.current_pos
 
-	def get_individual_values(self, tup):
-		# When tup is a tuple of values read from disk it is a single string (e.g. '1 Hello 7')
-		# when the relation has a signature R(a int, b text, c int), we want to get the individual values
-		# this mathod takes a sting '1 Hello 7' for R(a int, b text, c int) and returns a dictionary:
-		#	{'a':1, 'b':Hello, 'c':7}; as before, for simplicity strings can not have spaces
-		# we assume that the values are well typed
+        page = self.buffer.get_page(pname)
 
-		res = {}
+        # If the page position is empty, we call has_next again recursively:
+        if page.get_element_from_page(pos) == '<EMPTY>':
+            return self.get_next_pos()
+        
+        return True
 
-		vals = tup.split(' ')
-		for i in range(self.numAttrib):
-			attrib = self.attributes[i]
-			if (self.types[i] == 'int'):
-				res[attrib] = int(vals[i])
-			else:
-				res[attrib] = vals[i]
 
-		return res
+        
+    def next(self):
+        # returns the next tuple; if it does not exist returns none
+        # When called, will advance the self.current_page and self.current_pos pointers
+        if (self.get_next_pos()):
+            pname = self.current_page
+            page = self.buffer.get_page(pname)
+            return page.get_element_from_page(self.current_pos)
+        return None
 
-	def insert_tuple(self, tup):
-		# first run from first page, till the last one to check whether you have the tuple already
-		# on the way make sure to find the first empty position
 
-		# all is asked from the buffer
+    def close(self):
+        # Closes the iterator
+        self.current_page = None
+        self.current_pos = None
 
-		# then just call insert_into_pos using the buffer
-		# to save the changes also write the page!!!
+    def get_attribute_types(self):
+        # Returns a dictionary of attribute name: attribute type
+        # E.g. if we have R(a int, b str), it will return {'a':'int','b':'str'}
 
-		# If you come to the end, needs to create a new page and link it to the next one
+        res = {}
 
-		pass
+        for i in range(self.numAttrib):
+            res[self.attributes[i]] = self.types[i]
+
+        return res
+
+    def get_individual_values(self, tup):
+        # When tup is a tuple of values read from disk it is a single string (e.g. '1 Hello 7')
+        # when the relation has a signature R(a int, b text, c int), we want to get the individual values
+        # this mathod takes a sting '1 Hello 7' for R(a int, b text, c int) and returns a dictionary:
+        #    {'a':1, 'b':Hello, 'c':7}; as before, for simplicity strings can not have spaces
+        # we assume that the values are well typed
+
+        res = {}
+
+        vals = tup.split(' ')
+        for i in range(self.numAttrib):
+            attrib = self.attributes[i]
+            if (self.types[i] == 'int'):
+                res[attrib] = int(vals[i])
+            else:
+                res[attrib] = vals[i]
+
+        return res
+
+    def insert_tuple(self, tup):
+        # first run from first page, till the last one to check whether you have the tuple already
+        # on the way make sure to find the first empty position
+
+        # all is asked from the buffer
+
+        # then just call insert_into_pos using the buffer
+        # to save the changes also write the page!!!
+
+        # If you come to the end, needs to create a new page and link it to the next one
+
+        # reset self.current_page
+        self.open()
+
+        # Check if tuple exists in relation
+        while self.has_next():
+            current_tuple = self.next()
+            # If tuple exists, do not insert
+            if current_tuple == tup:
+                print(f"Can't insert tuple ({tup}), value is already present in {self.rName}")
+                self.close()
+                return
+
+        # Could not find tuple, find first empty space
+        # Get root page
+        current_page = self.buffer.get_page(self.root_page)
+        current_index = 1
+        # Check if root page has empty space
+        empty_index = current_page.has_empty()
+        while empty_index == -1:
+            # If it doesn't, check next page
+            current_index += 1
+            # If there isn't another page, create one
+            if current_page.next == '<NULL>':
+                new_page = self.buffer.create_page(self.rName + f'-{current_index}')
+                # Link new page
+                current_page.set_next_page(new_page.pname)
+                # Save old page (<NEXT> value)
+                self.buffer.evict_page(current_page.pname)
+                current_page = new_page
+            else:
+                # Get next page
+                current_page = self.buffer.get_page(self.rName + f'-{current_index}')
+            # Check if current page has empty space
+            empty_index = current_page.has_empty()
+        
+        # Insert tuple in found space
+        current_page.insert_into_pos(empty_index, tup)
+        # Save page
+        # self.buffer.flush_buffer()
+        # Close iterator
+        self.close()
+
+    def get_iterator(self):
+        return self.__iter__()
+
+    def __iter__(self):
+        return RelationIterator(self)
+
+
+class RelationIterator:
+
+    def __init__(self, relation):
+        from copy import copy
+        self.__relation = copy(relation)
+        self.open()
+
+    def __iter__(self):
+        return self
+
+    def open(self):
+        return self.__relation.open()
+
+    def __next__(self):
+        tup = self.__relation.next()
+        if tup == None:
+            raise StopIteration
+        return tup
+
+    def next(self):
+        try:
+            return next(self)
+        except StopIteration:
+            return None
+
+    def has_next(self):
+        return self.__relation.has_next()
+
+    def close(self):
+        return self.__relation.close()
+
+
 
 # Test case:
-'''
-buff = Buffer()
-R = Relation('R',['a','b'],['int','int'],buff)
 
-R.insert_tuple('11 153') # New tuple
-R.insert_tuple('27 1329') # Create a new next_page
-R.insert_tuple('11 153') # Insert existing tuple
-'''
+if __name__ == "__main__":
+    buff = Buffer()
+    R = Relation('R',['a','b'],['int','int'],buff)
+
+    R.insert_tuple('15 12') # New tuple
+    R.insert_tuple('27 1329') # Create a new next_page
+    R.insert_tuple('11 153') # Insert existing tuple
 
